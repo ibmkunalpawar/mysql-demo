c22a4a80fe8a44fdde1f6ee5fd12c9be677a4aaa
unknown <ibmjavanh13@iiht.tech>
2021-04-08 18:01:07 +0530

    Mysql demo

---
+++ src/main/java/com/example/mysqldemo/entity/User.java
@@ -0,0 +1,47 @@
+package com.example.mysqldemo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class User {
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String name;
+	@Email(message="Enter Email")
+	private String emailId;
+	@NotNull
+	@Min(1000000000)
+	@Max(9999999999L)
+	private long mobileNumber;
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public long getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(long mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+}
---
+++ src/main/java/com/example/mysqldemo/service/UserService.java
@@ -0,0 +1,16 @@
+package com.example.mysqldemo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.mysqldemo.entity.User;
+@Service
+public class UserService implements IUserService {
+	@Autowired
+	UserRepository userRepository;
+	@Override
+	public int save(User user) {
+		User savedUser = userRepository.save(user);
+		return savedUser.getId();
+	}
+}
